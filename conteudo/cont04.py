6da80b7a3e74a75bc4f0a82c40345dbfecc5927f
whz7psico <wheslley.machado@escola.pr.gov.br>
2026-03-11 11:41:04 -0300

    Create cont04.py

---
+++ conteudo/cont04.py
@@ -0,0 +1,7 @@
+#O + é utilizado como operador de adição
+#O - é utilizado como sinal de subtração
+#O * é utilizado para fazer a multiplicação
+#/ é utilizada para fazer a divisão
+#** é utilizado para fazer a exponenciação
+#// é utilizado para fazer a divisão inteira
+#% é utilizada para o resto da divisão
