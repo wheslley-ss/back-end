e869361bd4612d6cd2b57df7d01fdce9c6a9f507
whz7psico <wheslley.machado@escola.pr.gov.br>
2026-03-11 11:37:56 -0300

    Create ex05.py

---
+++ exercicio/ex05.py
@@ -0,0 +1,4 @@
+n = input('digite algo')
+print(n.isnumeric()) #para perguntar se é um número
+print(n.isalpha()) #para perguntar se é letra
+print(n.islower()) #para verificar se é apenas letras minusculas
