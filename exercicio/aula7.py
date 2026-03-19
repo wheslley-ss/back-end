641d3a45ddb3917d21fbf32567881a13d913aff4
whz7psico <wheslley.machado@escola.pr.gov.br>
2026-03-19 08:02:10 -0300

    Create aula7.py

---
+++ exercicio/aula7.py
@@ -0,0 +1,6 @@
+n1 = int(input('Digite um número: '))
+
+print('o número digitado foi {} o seu dobro é {}, seu triplo {} e sua raiz quadrada é {:.2f}'
+      .format(n1, n1 * 2, n1 * 3, n1 ** 0.5))
+
+# Para calcular a raiz quadrada, você também pode usar a função pow(n1, 0.5)
