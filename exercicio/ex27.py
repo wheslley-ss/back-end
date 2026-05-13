000e0ab50695256790c839b65590554f4fe98310
whz7psico <wheslley.machado@escola.pr.gov.br>
2026-05-13 09:39:58 -0300

    Create ex27.py

---
+++ exercicio/ex27.py
@@ -0,0 +1,5 @@
+nome = str(input("Digite seu nome completo")).strip()
+dividido = nome.split()
+print("Muito prazer em te conhecer!")
+print("Seu primeiro nome é {}".format(dividido [0]))
+print("Seu ultimo nome é {}".format(dividido [len (dividido)-1]))
