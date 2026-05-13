9d9e523599ce045e17bbadd8c3a7ddaee3527fda
whz7psico <wheslley.machado@escola.pr.gov.br>
2026-05-13 09:34:31 -0300

    Create ex17.py

---
+++ exercicio/ex17.py
@@ -0,0 +1,8 @@
+import math
+n1 = float(input("Digite um número"))
+print("o valor digitado foi {} e a sua fração inteira é {}".format(n1,math.trunc(n1)))
+from math import trunc
+n1 = float(input("Digite um número"))
+print("o valor digitado foi {} e a sua fração inteira é {}".format(n1, trunc(n1)))
+n1 = float(input("Digite um número"))
+print("o valor digitado foi {} e a sua fração inteira é {}".format(n1,int(n1)))
