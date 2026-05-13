d5039aaa44f210dce455c0b2df613e8dbbacd38b
whz7psico <wheslley.machado@escola.pr.gov.br>
2026-05-13 09:42:10 -0300

    Create ex29.py

---
+++ exercicio/ex29.py
@@ -0,0 +1,7 @@
+velocidade float(input("Qual é a velocidade atual do carro? "))
+multa (velocidade-80)*7
+if velocidade<=88:
+print("Tenha um bom dia! Dirija com segurança")
+else:
+print("Multado! Você excedeu o limite permitido que é de 80Km/h. Você deve pagar uma multa de R${:.2f}".format
+(multa))
