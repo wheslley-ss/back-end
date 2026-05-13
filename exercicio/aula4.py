88cf55bc4a30933f4c296d4e36a55c3ceb6cbf44
whz7psico <wheslley.machado@escola.pr.gov.br>
2026-05-13 09:44:23 -0300

    Create aula4.py

---
+++ exercicio/aula4.py
@@ -0,0 +1,5 @@
+num1 = int(input('digite um número'))
+num2 = int(input('digite outro número'))
+soma num1 + num2
+#print('a soma entre () format(num1),'é () .format(num2), é ()'.format(soma))
+print('a soma entre {} e {} é {}'.format(num1, num2, soma))
